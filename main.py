5f19e324a039ebbe146baad7053de9565fc30e1c
marcus-tam <marcusjtam1@gmail.com>
2021-01-06 23:48:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 # Written By Marcus Tam
 """
-    Basic selenium application to trade stocks on WealthSimple. 
-
+    Basic selenium web automation application to trade stocks on WealthSimple. 
+    This application is for education purposes.
     This is meant to be a personal application, but feel free to use and modify. If you notice any glaring errors, please write an issue
     and I'll be sure to get to it soon. 
-
-    This application is for education purposes. I (Marcus Tam) do not take responsibility of you gain or lose money if you choose to use 
-    this application. 
 """
 
 import schedule
